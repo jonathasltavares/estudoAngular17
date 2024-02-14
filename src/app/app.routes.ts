355fcdc29f1ad19a2d20e6b3850f349492f2c2f5
Jonathas Tavares <jonathasl.tavares@gmail.com>
2024-02-14 15:50:46 -0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
+import path from 'path';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: HomeComponent }];
